d340cb9db0424cac5abdb068ae985a935360be4d
Ida Taaki <47431356+idataaki@users.noreply.github.com>
2023-04-30 05:54:23 +0000

    compare.py

--- compare.py
+++ compare.py
@@ -1,1 +1,17 @@
-print('hello')
+def make_confiusion_matrix():
+    # make confusion matrix
+    pass
+def calc_acuracy_score():
+    # get acuracy score
+    pass
+def calc_log_loss():
+    # get log loss
+    pass
+
+# real files/labels.txt and store it
+
+# read detections/detr_det.txt
+
+# read detections/yolov5_det.txt
+
+# read detections/yolov8_det.txt
